4b06b29532a470d9e5cd1b37971b106b6e301762
Kumar Ravi <techsavy.ravi@gmail.com>
2021-01-16 16:59:09 +0530

    otp daily

--- daily_otp_override.py
+++ daily_otp_override.py
@@ -1,7 +1,10 @@
 from random import randint
-import subprocess, os
+import os
+from slack import send2SlackCustomURL
 
 otp = randint(100000, 999999)
 
 os.system("heroku config:set OTP=" + str(otp) + " --app b2c-user-auth-ms-staging")
 os.system("heroku config:set OTP=" + str(otp) + " --app b2c-user-auth-ms")
+
+send2SlackCustomURL("OTP for Today", str(otp), "https://hooks.sla ck.com/services/TPMAJ1G13/B01KA6R5E73/tmM1NBwnuCItDJZiem7FrxiW")
