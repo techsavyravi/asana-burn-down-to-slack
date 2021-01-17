40434d1af7d9c8df55bc05eac9fc2746f6d1dad3
Kumar Ravi <techsavy.ravi@gmail.com>
2021-01-17 18:56:49 +0530

    syncservice

---
+++ syncService.py
@@ -0,0 +1,18 @@
+from dataSync import DAU
+import os
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+MONGO_URL = os.getenv('MONGO_URL')
+MONGO_DRS_URL = os.getenv('MONGO_DRS_URL')
+
+client = MongoClient(os.environ['MONGO_URL'])
+
+change_stream = client.iDreamCareer.users.watch([
+      { "$match" : {"operationType" : "insert" } }
+   ])
+
+for change in change_stream:
+    print(dumps(change))
+    print('') # for readability only
+    DAU()
